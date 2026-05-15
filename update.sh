bdc869aa3fa0060f855e2efcf51afd7ae24d0bc8
Christopher Bates <originalme8@gmail.com>
2026-05-15 11:43:10 -0700

    Changes to be committed: 	modified:   update.sh         - sourcing tools.func at start

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/tools.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: MickLesk (CanbiZ)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
